8eaaf73a55e83ac9975bd389e181b08e51b41885
Adarsh <adarshgupta2468@gmail.com>
2022-01-16 08:37:28 +0530

    mathews porftolio linear page

--- src/components/Enlargecomponents/TypeOf.jsx
+++ src/components/Enlargecomponents/TypeOf.jsx
@@ -4,7 +4,7 @@ import typeofi from "../../images/typeof.svg"
 function TypeOf() {
     return (
         <div className='bg-red-200 w-full h-full' style={{height:"100vh"}}>
-            <img src={typeofi} className=' style={{height:"100vh"}}/>
+            <img src={typeofi} className='' style={{height:"100vh"}}/>
         </div>
     )
 }
